4149093241889b4ddcef524bb85db8039f08f6ae
Nick Grey <soundsnick@gmail.com>
2018-03-20 11:30:00 +0600

    This is gonna fix the font v4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+#
+Add the fonts path
+config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
-Rails.application.config.assets.precompile += ['*.js', '*.css', '**/*.js', '**/*.css']
-
-config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+Precompile additional assets
+config.assets.precompile += %w( .svg .eot .woff .ttf )
